e1b72791f56d93a3cc2b1f694df74267c7ea1872
vmarois <vincent.marois@protonmail.com>
2019-03-22 13:54:28 -0400

    Add initial version of Encoder Layer.

---
+++ transformer/encoder.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from transformer.utils import clones
+from transformer.layers import ResidualConnection
+
+
+class EncoderLayer(nn.Module):
+    """
+    Implements one Encoder layer. The actual Encoder is a stack of N of these layers.
+
+    The overall forward pass of this layer is as follows:
+
+    input x -> Self-Attention -> Sum -> LayerNorm -> FeedForward -> Sum -> LayerNorm -> output
+            |                            ^                |                  ^
+            v -------------------------> |                v ---------------- |
+    """
+
+    def __init__(self, size:int, self_attn: nn.Module, feed_forward: nn.Module, dropout: float):
+        """
+        Constructor for the ``EncoderLayer`` class.
+
+        :param size: Input size.
+
+        :param self_attn: Class used for the self-attention part of the layer.
+
+        :param feed_forward: Class used for the feed-forward part of the layer.
+
+        :param dropout: dropout probability
+
+        """
+        # call base constructor
+        super(EncoderLayer, self).__init__()
+
+        # get self-attn & feed-forward sub-modules
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+
+        self.sublayer = clones(ResidualConnection(size, dropout), 2)
+
+    def forward(self, x: Tensor, mask=None) -> Tensor:
+        """
+        Implements the forward pass of the Encoder layer.
+
+        :param x: Input Tensor, should be 3-dimensional: (batch_size, seq_length, d_model).
+                Should represent the input sentences or the output of the previous Encoder layer.
+
+        :param mask: Mask to be used in the self-attention sub-module. Optional.
+
+        :return: Output of the EncoderLayer, should be of the same shape as the input.
+
+        """
+        attn_out = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+
+        return self.sublayer[1](attn_out, self.feed_forward)
+
+
+if __name__ == '__main__':
+
+    from transformer.attention import MultiHeadAttention
+    from transformer.layers import PositionwiseFeedForward
+
+    enc_layer = EncoderLayer(size=512, self_attn=MultiHeadAttention(n_head=8, d_model=512, d_k=64, d_v=64, dropout=0.1),
+                             feed_forward=PositionwiseFeedForward(d_model=512, d_ff=2048, dropout=0.1), dropout=0.1)
+
+    x = torch.ones((64, 10, 512))
+
+    out = enc_layer(x)
+
+    print(out.shape)
