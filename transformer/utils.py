b6b8eab9ffc39c871225c082587eb4fc925a3257
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2019-03-23 16:56:43 -0400

    add subsequent mask in utils

--- transformer/utils.py
+++ transformer/utils.py
@@ -1,4 +1,6 @@
 import copy
+import torch
+import numpy as np
 import torch.nn as nn
 
 
@@ -7,6 +9,18 @@ def clones(module, N):
     Produce ``N`` identical copies of ``module`` and return them as a ``nn.ModuleList``.
     """
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def subsequent_mask(size):
+    """
+    Mask out subsequent positions.
+    :param size: Input size
+    :return: Tensor with boolean mask on subsequent position
+    """
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
 
 
 class BColors:
