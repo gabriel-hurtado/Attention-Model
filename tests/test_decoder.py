3d231b6ab811e8d76acec9892613c217b67d0481
Alexandre Jouandin <alexandre@jouand.in>
2019-03-23 20:16:59 -0400

    Clean-up Unit-Test

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -15,35 +15,33 @@ class TestDecoderLayer(TestCase):
         sequence_length = 10
         d_k = d_v = d_model = input_size = 512
         d_ff = 2048
-        N_decoder_layer = 6
+        nb_of_decoder_layers = 6
 
-        # initialization decoder
-        decoder_layer = DecoderLayer(size=input_size,
-                                     self_attn=MultiHeadAttention(n_head=8, d_model=d_model,
-                                                                  d_k=d_k, d_v=d_v,
-                                                                  dropout=0.1),
-                                     memory_attn=MultiHeadAttention(n_head=8, d_model=d_model,
-                                                                    d_k=d_k, d_v=d_v,
-                                                                    dropout=0.1),
-                                     feed_forward=PositionwiseFeedForward(d_model=d_model,
-                                                                          d_ff=d_ff,
-                                                                          dropout=0.1),
-                                     dropout=0.1)
+        # Initialize decoder
+        decoder_layer = DecoderLayer(
+            size=input_size,
+            self_attn=MultiHeadAttention(n_head=8, d_model=d_model,
+                                         d_k=d_k, d_v=d_v, dropout=0.1),
+            memory_attn=MultiHeadAttention(n_head=8, d_model=d_model,
+                                           d_k=d_k, d_v=d_v, dropout=0.1),
+            feed_forward=PositionwiseFeedForward(d_model=d_model, d_ff=d_ff, dropout=0.1),
+            dropout=0.1
+        )
 
-        decoder = Decoder(layer=decoder_layer, N=N_decoder_layer)
+        decoder = Decoder(layer=decoder_layer, N=nb_of_decoder_layers)
 
-        # initialization input and memory
+        # Initialize input and memory
         x = torch.ones((batch_size, sequence_length, input_size))
         memory = torch.ones((batch_size, sequence_length, input_size))
 
-        # subsequent mask: mask all words > i
+        # Subsequent mask: mask all words with length > i
         decoder_mask = subsequent_mask(sequence_length)
 
-        # forward pass with fool input and memory (same here)
-        out = decoder(x, memory, decoder_mask, None)
+        # Forward pass with fool input and memory (same here)
+        out = decoder.forward(x, memory, decoder_mask, None)
 
-        # unit test
-        self.assertEqual(out.shape, x.shape)
+        # Unit Tests
         self.assertIsInstance(out, torch.Tensor)
+        self.assertEqual(out.shape, x.shape)
         self.assertEqual(out.shape, memory.shape)
         self.assertEqual(x.shape, memory.shape)
