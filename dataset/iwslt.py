cdeb6930fb4d10dd6253737bb1047a8e9d75c3fc
Alexandre Jouandin <alexandre@jouand.in>
2019-04-03 00:32:05 -0400

    Move to one root dataset
    + Added calls to IWSLT dataset superclass initialiser
    + Renamed some ‘’language’’ variables to ‘’language_pair’’ to be more explicit.

--- dataset/iwslt.py
+++ dataset/iwslt.py
@@ -6,12 +6,14 @@ from torchtext import data, datasets
 from dataset.europarl import Split
 from dataset.utils import Tokenizer
 
+ROOT_DATASET_DIR="resources/torchtext"
 
-class IWSLTLanguage(IntEnum):
+
+class IWSLTLanguagePair(IntEnum):
     fr_en = auto()
 
     def tokenizer(self):
-        if self == IWSLTLanguage.fr_en:
+        if self == IWSLTLanguagePair.fr_en:
             return (
                 Tokenizer(language='fr'),
                 Tokenizer(language='en'),
@@ -20,62 +22,54 @@ class IWSLTLanguage(IntEnum):
             raise ValueError()
 
     def extensions(self):
-        if self == IWSLTLanguage.fr_en:
-            return (
-                '.fr',
-                '.en'
-            )
-        else:
-            raise ValueError()
-
-    def path(self):
-        if self == IWSLTLanguage.fr_en:
-            return "resources/IWSLT/fr-en/"
+        if self == IWSLTLanguagePair.fr_en:
+            return ('.fr', '.en')
         else:
             raise ValueError()
 
 
 class IWSLT(data.Dataset):
-    def __init__(
-        self,
-        language: IWSLTLanguage,
-        split: Split,
-        split_ratio=0.6,
-        random_state=100,
-        b_of_sequence_word='<s>',
-        e_of_sequence_word='</s>',
-        blank_word="<blank>",
-    ):
-        # load corresponding tokenizer
-        source_tokenizer, target_tokenizer = language.tokenizer()
-        # load corresponding extensions for IWSLT loading
-        source_extension, target_extension = language.extensions()
-        # path to dataset (if already downloaded)
-        self.path = language.path()
-        # create pytorchtext data field to generate vocabulary
-        self.source_field = data.Field(tokenize=source_tokenizer, pad_token=blank_word)
-        self.target_field = data.Field(tokenize=target_tokenizer, init_token=b_of_sequence_word,
-                                       eos_token=e_of_sequence_word, pad_token=blank_word)
+    def __init__(self, language_pair: IWSLTLanguagePair, split: Split, split_ratio=0.6,
+                 random_state=100, start_token="<s>", eos_token="</s>",
+                 blank_token="<blank>", max_length=100):
+        """
+        Initializes an IWSLT dataset.
 
-        # max length of sequence
-        max_length = 100
+        :param language_pair: The language pair for which to create a vocabulary.
+        :param split: The split type.
+        :param split_ratio: The size of the split
+        :param random_state: The random seed to use.
+        :param start_token: The token that marks the beginning of a sequence.
+        :param eos_token: The token that marks an end of sequence.
+        :param blank_token: The token to pad with.
+        :param max_length: Max length of sequence.
+        """
+        # load corresponding tokenizer
+        source_tokenizer, target_tokenizer = language_pair.tokenizer()
+        # create pytorchtext data field to generate vocabulary
+        self.source_field = data.Field(tokenize=source_tokenizer, pad_token=blank_token)
+        self.target_field = data.Field(tokenize=target_tokenizer, init_token=start_token,
+                                       eos_token=eos_token, pad_token=blank_token)
 
         # split dataset by loading corresponding source and target files from .data/ folder
         train, validation, test = datasets.IWSLT.splits(
             split_ratio=split_ratio,
             random_state=random_state,
-            check=self.path,  # check if dataset already downloaded
-            exts=(source_extension, target_extension),
+            root=ROOT_DATASET_DIR,  # To check if dataset already downloaded
+            exts=language_pair.extensions(),
             fields=(self.source_field, self.target_field),
             filter_pred=lambda x: len(vars(x)['src']) <= max_length
-                                  and len(vars(x)['trg']) <= max_length)
+                                  and len(vars(x)['trg']) <= max_length
+        )
 
         if split == Split.Train:
-            self.array = train
+            examples = train
         elif split == Split.Validation:
-            self.array = validation
+            examples = validation
         else:
             raise NotImplementedError()
+        super().__init__(examples=examples,
+                         fields=[("source", self.source_field), ("target", self.target_field)])
 
     def build_vocabulary(self):
         min_freq = 2
@@ -83,11 +77,3 @@ class IWSLT(data.Dataset):
         # take some times
         #self.source_field.build_vocab(train.src, min_freq=min_freq)
         #self.target_field.build_vocab(train.trg, min_freq=min_freq)
-
-    def __getitem__(self, index):
-        source, target = self.array[index]
-        print(source)
-        return source, target
-
-    def __len__(self):
-        return len(self.indexes)
--- tests/test_iwslt.py
+++ tests/test_iwslt.py
@@ -1,17 +1,18 @@
 from unittest import TestCase
 
-from dataset.iwslt import IWSLT, IWSLTLanguage
+from dataset.iwslt import IWSLT, IWSLTLanguagePair
 from dataset.utils import Split
+
 
 class TestIWSLT(TestCase):
     def test_getitem(self):
         batch_size = 64
+        language = IWSLTLanguagePair.fr_en
 
-        source, targ = IWSLTLanguage.fr_en.tokenizer()
-        dataset = IWSLT(language=IWSLTLanguage.fr_en, split=Split.Train, split_ratio=0.6)
+        source, targ = language.tokenizer()
+        dataset = IWSLT(language_pair=language, split=Split.Train, split_ratio=0.6)
         source, target = dataset[0]
 
         # unit tests
         self.assertIsInstance(source, str)
         self.assertIsInstance(target, str)
-
