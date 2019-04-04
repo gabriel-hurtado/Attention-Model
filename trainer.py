b0d3d0843016de466f3bcae7ad5b68aa7b1b47c0
vmarois <vincent.marois@protonmail.com>
2019-04-04 12:58:31 -0400

    Initial trainer class. Dummy for now.

---
+++ trainer.py
@@ -0,0 +1,31 @@
+import torch
+from transformer.model import Transformer
+
+
+class Trainer(object):
+    """
+    Represents a worker taking care of the training of an instance of the ``Transformer`` model.
+
+    """
+
+    def __init__(self, params):
+        """
+        Constructor of the Trainer.
+        Sets up the following:
+            - Device available (e.g. if CUDA is present)
+            - Initialize the model, dataset, loss, optimizer
+            - log statistics (epoch, elapsed time, BLEU score etc.)
+        """
+
+        # if CUDA available, moves computations to GPU
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        else:
+            device = torch.device('cpu')
+
+        # instantiate model
+        model = Transformer(params["model"]).to(device)
+
+    def train(self):
+        pass
+
