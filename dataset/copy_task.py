636f029fcb3d109a2e67b55f534422c733d214c4
vmarois <vincent.marois@protonmail.com>
2019-04-05 16:46:59 -0400

    Added a small Batch class along the CopyTask class.

--- dataset/copy_task.py
+++ dataset/copy_task.py
@@ -1,10 +1,12 @@
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data.dataloader import default_collate
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
+
 
 class CopyTaskDataset(Dataset):
     """
@@ -45,3 +47,18 @@ class CopyTaskDataset(Dataset):
         sample = torch.randint(low=0, high=self.max_int, size=(self.max_seq_length,), device=device)
 
         return sample, sample
+
+    def collate(self, samples):
+        inputs, targets = default_collate(samples)
+
+        return Batch(inputs, targets)
+
+
+class Batch:
+    def __init__(self, src_sequences, tgt_sequences):
+        self.src_sequences = src_sequences
+        self.tgt_sequences = tgt_sequences
+
+    def cuda(self):
+        self.src_sequences = self.src_sequences.cuda()
+        self.tgt_sequences = self.tgt_sequences.cuda()
