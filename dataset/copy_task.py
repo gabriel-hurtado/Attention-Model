50008dc7b24543ea64f386797c1be685c31be237
vmarois <vincent.marois@protonmail.com>
2019-04-05 16:00:10 -0400

    Added a small CopyTask dataset class for testing the model.

---
+++ dataset/copy_task.py
@@ -0,0 +1,47 @@
+import torch
+from torch.utils.data import Dataset
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+class CopyTaskDataset(Dataset):
+    """
+    A simple copy task: The network is asked to produce outputs similar to the inputs.
+
+    This is used as a basic test to ensure gradients are flowing correctly through the network, and the latter
+    is able to overfit a small amount of data.
+    """
+    def __init__(self, max_int: int, max_seq_length: int, size: int,):
+        """
+        Constructor of the ``CopyTaskDataset``.
+
+        :param max_int: Upper bound on the randomly drawn samples.
+
+        :param max_seq_length: Sequence length. Will be the same for all samples.
+
+        :param size: Size of the dataset. Mainly used for ``__len__`` as generated samples are random (and bounded by ``max_len``).
+        """
+        self.max_int = max_int
+        self.max_seq_length = max_seq_length
+        self.size = size
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, item):
+        """
+        Randomly creates a sample of shape [1, self.max_seq_length], where the elements are drawn randomly
+        in U[0, self.max_int].
+
+        As this is a copy task, inputs = targets.
+
+        :param item: index of the sample, not used here.
+
+        :return: tuple (inputs, targets) of identical shape.
+        """
+
+        sample = torch.randint(low=0, high=self.max_int, size=(self.max_seq_length,), device=device)
+
+        return sample, sample
