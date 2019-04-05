4c1075ece49dadaef73bb02cdb8bb2bf154f08c7
vmarois <vincent.marois@protonmail.com>
2019-04-05 08:12:46 -0400

    Added logging capability.

--- trainer.py
+++ trainer.py
@@ -1,4 +1,9 @@
+import os
 import torch
+import logging
+import logging.config
+from datetime import datetime
+
 from transformer.model import Transformer
 from training.optimizer import NoamOpt
 from training.loss import LabelSmoothingLoss
@@ -39,8 +44,86 @@ class Trainer(object):
 
         # missing dataloader
 
-        # missing logger
+        # configure all logging
+        self.configure_logging(training_problem_name="copy_task")
+
+        self.logger.info('Experiment setup done.')
 
     def train(self):
         pass
+
+    def configure_logging(self, training_problem_name) -> None:
+        """
+        Takes care of the initialization of logging-related objects:
+
+            - Sets up a logger with a specific configuration,
+            - Sets up a logging directory
+            - sets up a logging file in the log directory
+            - Sets up a folder to store trained models
+
+        :param training_problem_name: Name of the dataset / training task (e.g. "copy task", "IWLST"). Used for the logging
+        folder name.
+        """
+        # instantiate logger
+        # Load the default logger configuration.
+        logger_config = {'version': 1,
+                         'disable_existing_loggers': False,
+                         'formatters': {
+                             'simple': {
+                                 'format': '[%(asctime)s] - %(levelname)s - %(name)s >>> %(message)s',
+                                 'datefmt': '%Y-%m-%d %H:%M:%S'}},
+                         'handlers': {
+                             'console': {
+                                 'class': 'logging.StreamHandler',
+                                 'level': 'INFO',
+                                 'formatter': 'simple',
+                                 'stream': 'ext://sys.stdout'}},
+                         'root': {'level': 'DEBUG',
+                                  'handlers': ['console']}}
+
+        logging.config.dictConfig(logger_config)
+
+        # Create the Logger, set its label and logging level.
+        self.logger = logging.getLogger(name='Trainer')
+
+        # Prepare the output path for logging
+        time_str = '{0:%Y%m%d_%H%M%S}'.format(datetime.now())
+
+        self.log_dir = 'experiments/' + training_problem_name + '/' + time_str + '/'
+
+        # create folder
+        os.makedirs(self.log_dir, exist_ok=False)
+
+        # Set log dir and add the handler for the logfile to the logger.
+        self.log_file = self.log_dir + 'training.log'
+        self.add_file_handler_to_logger(self.log_file)
+
+        # Models dir: to store the trained models.
+        self.model_dir = self.log_dir + 'models/'
+        os.makedirs(self.model_dir, exist_ok=False)
+
+    def add_file_handler_to_logger(self, logfile):
+        """
+        Add a ``logging.FileHandler`` to the logger.
+
+        Specifies a ``logging.Formatter``:
+            >>> logging.Formatter(fmt='[%(asctime)s] - %(levelname)s - %(name)s >>> %(message)s',
+            >>>                   datefmt='%Y-%m-%d %H:%M:%S')
+
+        :param logfile: File used by the ``FileHandler``.
+
+        """
+        # create file handler which logs even DEBUG messages
+        fh = logging.FileHandler(logfile)
+
+        # set logging level for this file
+        fh.setLevel(logging.DEBUG)
+
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter(fmt='[%(asctime)s] - %(levelname)s - %(name)s >>> %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+        fh.setFormatter(formatter)
+
+        # add the handler to the logger
+        self.logger.addHandler(fh)
 
